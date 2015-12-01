6bb164d9b77996e466c8250ee52ddc75ae74c631
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2015-12-01 23:06:21 +0100

    pyacswui installer - more checks for database tables

--- pyacswui/installer.py
+++ pyacswui/installer.py
@@ -9,18 +9,6 @@ class Installer(object):
             Create table if not existent and set index.
         """
 
-        # retype parameters
-        if idxdefault is not None:
-            idxdefault = "DEFAULT %s" % idxdefault
-        else:
-            idxdefault = ""
-
-        if idxonupdate is not None:
-            idxonupdate = "ON UPDATE %s" % idxonupdate
-        else:
-            idxonupdate = ""
-
-
         # check if table already exist
         table_exist = False
         query = "SELECT `TABLE_NAME` FROM information_schema.TABLES WHERE table_schema = '%s';" % self.__args.db_database
@@ -33,22 +21,90 @@ class Installer(object):
                 table_exist = True
         cursor.close()
 
-        if table_exist is True:
-            return
-
-
         # create query
-        query = "CREATE TABLE IF NOT EXISTS `%s` (`%s` %s NOT NULL %s %s) ENGINE=InnoDB DEFAULT CHARSET=latin1;" % (tblname, idxname, idxtype, idxdefault, idxonupdate)
-        if self.__args.v > 1:
-            print("    " + query)
+        if table_exist is False:
 
-        # execute query
-        cursor = self.__db_handle.cursor()
-        cursor.execute(query)
-        cursor.close()
+            # retype parameters
+            if idxdefault is not None:
+                idxdefault = "DEFAULT %s" % idxdefault
+            else:
+                idxdefault = ""
+
+            if idxonupdate is not None:
+                idxonupdate = "ON UPDATE %s" % idxonupdate
+            else:
+                idxonupdate = ""
+
+            query = "CREATE TABLE IF NOT EXISTS `%s` (`%s` %s NOT NULL %s %s) ENGINE=InnoDB DEFAULT CHARSET=latin1;" % (tblname, idxname, idxtype, idxdefault, idxonupdate)
+            if self.__args.v > 1:
+                print("    " + query)
+
+            # execute query
+            cursor = self.__db_handle.cursor()
+            cursor.execute(query)
+            cursor.close()
+
+        # table already exist
+        else:
+            # ensure to have index column
+            self._dbAppendColumn(tblname, idxname,idxtype, idxdefault, idxonupdate)
+
+            # check index
+            primary_index_found = False
+            primary_index_correct = False
+            query = "SHOW INDEX FROM %s WHERE Key_name = 'PRIMARY';" % tblname
+            if self.__args.v > 2:
+                print("  " + query)
+            cursor = self.__db_handle.cursor()
+            cursor.execute(query)
+            for r in cursor.fetchall():
+                if r[2].lower() == "primary":
+                    primary_index_found = True
+                    if r[4] == idxname:
+                        primary_index_correct = True
+            cursor.close()
+
+            if primary_index_correct is False:
+                if primary_index_found is True:
+                    query = "ALTER TABLE `%s` DROP PRIMARY KEY, ADD PRIMARY KEY(`%s`);" % (tblname, idxname)
+                else:
+                    query = "ALTER TABLE `%s` ADD PRIMARY KEY(`%s`);" % (tblname, idxname)
+                if self.__args.v > 1:
+                    print("    " + query)
+                # execute query
+                cursor = self.__db_handle.cursor()
+                cursor.execute(query)
+                cursor.close()
+
+
 
 
     def _dbAppendColumn(self, tblname, colname, coltype, coldefault = None, colonupdate = None):
+
+        # assume column exist already
+        column_exist = False
+        column_needs_change = False
+
+        # check column info
+        query = "SELECT `COLUMN_NAME`, `COLUMN_TYPE`, `COLUMN_DEFAULT`, `EXTRA` FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s' AND COLUMN_NAME = '%s';" % (self.__args.db_database, tblname, colname)
+        if self.__args.v > 2:
+            print("  " + query)
+        cursor = self.__db_handle.cursor()
+        cursor.execute(query)
+        if cursor.rowcount > 0:
+            column_exist = True
+            res = cursor.fetchall()[0]
+            if self.__args.v > 2:
+                print("    sql result: COLUMN_NAME=%s, COLUMN_TYPE=%s, COLUMN_DEFAULT=%s, EXTRA=%s" % (res[0], res[1], res[2], res[3]))
+            if res[0] != colname:
+                column_needs_change = True
+            if coltype is not None and res[1].lower() != coltype.lower():
+                column_needs_change = True
+            if coldefault is not None and res[2].lower() != coldefault.lower():
+                column_needs_change = True
+            if colonupdate is not None and ("ON UPDATE %s" % colonupdate).lower() not in res[3].lower():
+                column_needs_change = True
+        cursor.close()
 
         # retype parameters
         if coldefault is not None:
@@ -60,18 +116,6 @@ class Installer(object):
             colonupdate = "ON UPDATE %s" % colonupdate
         else:
             colonupdate = ""
-
-
-        # check if column already exist
-        column_exist = False
-        query = "SELECT `COLUMN_NAME`, `COLUMN_DEFAULT`, `DATA_TYPE`, `EXTRA` FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s' AND COLUMN_NAME = '%s'" % (self.__args.db_database, tblname, colname)
-        if self.__args.v > 2:
-            print("  " + query)
-        cursor = self.__db_handle.cursor()
-        cursor.execute(query)
-        if cursor.rowcount > 0:
-            column_exist = True
-        cursor.close()
 
         # create column
         if column_exist is False:
@@ -86,7 +130,7 @@ class Installer(object):
             cursor.close()
 
         # update column
-        else:
+        elif column_needs_change is True:
             # create query
             query = "ALTER TABLE `%s` CHANGE `%s` `%s` %s NOT NULL %s %s;" % (tblname, colname, colname, coltype, coldefault, colonupdate)
             if self.__args.v > 1:
