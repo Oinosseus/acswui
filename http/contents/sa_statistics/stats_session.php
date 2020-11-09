ecef5cea90915c0b2749080053199f2c807179bd
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2020-11-09 20:44:48 +0100

    sort session statistcs lap table by laptime

--- http/contents/sa_statistics/stats_session.php
+++ http/contents/sa_statistics/stats_session.php
@@ -125,25 +125,52 @@ class stats_session extends cContentPage {
         //                               Laps
         // --------------------------------------------------------------------
 
-        $html .= "<h1>" . _("Laps") . "</h1>";
+        $html .= "<h1>" . _("All Laps") . "</h1>";
         $html .= '<table>';
         $html .= '<tr><th>' . _("Lap") . '</th><th>' . _("Laptime") . '</th><th>' . _("Cuts") . '</th><th>' . _("Driver") . '</th><th>' . _("Car") . '</th><th>' . _("Grip") . '</th>';
-        $laps = $acswuiDatabase->fetch_2d_array("Laps", ['User', 'Laptime', 'Cuts', 'Grip', 'CarSkin'], ['Session'=>$this->SessionId], "Id", FALSE);
-        $lap_idx = count($laps);
-        foreach ($laps as $row) {
+        $laps = $acswuiDatabase->fetch_2d_array("Laps", ['User', 'Laptime', 'Cuts', 'Grip', 'CarSkin', 'Id'], ['Session'=>$this->SessionId], "Id", FALSE);
 
-            $class = ($row['Cuts'] > 0) ? "class=\"lap_invalid\"" : "";
+        // determine minimum lap id
+        $laps_min_id = Null;
+        foreach ($laps as $l) {
+            if ($laps_min_id === NUll || $l['Id'] < $laps_min_id) $laps_min_id = $l['Id'];
+        }
+
+        // find latest lap per driver
+        $latest_lap_drivers = array();
+        for ($i=0; $i < count($laps); ++$i) {
+            if (in_array($laps[$i]['User'], $latest_lap_drivers)) {
+                $laps[$i]['LatestLap'] = FALSE;
+            } else {
+                $laps[$i]['LatestLap'] = TRUE;
+                $latest_lap_drivers[] = $laps[$i]['User'];
+            }
+        }
+
+
+        // sort best lap times
+        function compare_laptimes($l1, $l2) {
+            return ($l1['Laptime'] < $l2['Laptime']) ? -1 : 1;
+        }
+        uasort($laps, 'compare_laptimes');
+
+
+        // dump laps
+        foreach ($laps as $l) {
+
+            $class = "class=\"";
+            $class .= ($l['Cuts'] > 0) ? " lap_invalid" : "";
+            $class .= ($l['LatestLap']) ? " lap_latest" : "";
+            $class .= "\"";
 
             $html .= "<tr $class>";
-            $html .= "<td>$lap_idx</td>";
-            $html .= "<td>" . laptime2str($row['Laptime']) . "</td>";
-            $html .= "<td>" . $row['Cuts'] . "</td>";
-            $html .= "<td>" . $drivers[$row['User']] . "</td>";
-            $html .= "<td>" . $this->getCarName($row['CarSkin']) . "</td>";
-            $html .= "<td>" . (100 * $row['Grip']) . "&percnt;</td>";
+            $html .= "<td>" . ($l['Id'] - $laps_min_id + 1) . "</td>";
+            $html .= "<td>" . laptime2str($l['Laptime']) . "</td>";
+            $html .= "<td>" . $l['Cuts'] . "</td>";
+            $html .= "<td>" . $drivers[$l['User']] . "</td>";
+            $html .= "<td>" . $this->getCarName($l['CarSkin']) . "</td>";
+            $html .= "<td>" . (100 * $l['Grip']) . "&percnt;</td>";
             $html .= '</tr>';
-
-            $lap_idx -= 1;
         }
         $html .= '</table>';
 
