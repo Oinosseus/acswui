7f18ece8350e26593ae3b47573b2c19d32264fb6
thomas <Thomas.Weinhold@stratoinos.de>
2021-01-17 23:15:29 +0100

    correct session permission

--- http/contents/zm_session.php
+++ http/contents/zm_session.php
@@ -5,7 +5,7 @@ class zm_session extends cContentPage {
     public function __construct() {
         $this->MenuName   = _("Session");
         $this->TextDomain = "acswui";
-        $this->RequirePermissions = ["View_ServerContent", "Session_View"];
+        $this->RequirePermissions = ["Session_View"];
     }
 
     public function getHtml() {
