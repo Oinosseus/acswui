38c48cbcc49be8e541b4583a516b9a8cc2b1c2f4
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2021-05-29 14:24:31 +0200

    show car classes overview

--- http/contents/bm_car_classes.php
+++ http/contents/bm_car_classes.php
@@ -9,7 +9,17 @@ class bm_car_classes extends cContentPage {
     }
 
     public function getHtml() {
-        return "";
+        $html = "";
+
+        foreach (CarClass::listClasses() as $cc) {
+
+            $html .= "<div class=\"CarClassBox\">";
+            $html .= "<label>" . $cc->name() . "</label>";
+            $html .= $cc->htmlImg("", 200);
+            $html .= "</div>";
+        }
+
+        return $html;
     }
 }
 
