6aef3e5c1c800ec9515a4cc48c9288c1de985d42
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2022-10-11 23:15:34 +0200

    correct default secific power unit

--- http/classes/Parameter/ParamSpecialUserPowerSpecific.php
+++ http/classes/Parameter/ParamSpecialUserPowerSpecific.php
@@ -7,8 +7,8 @@ class ParamSpecialUserPowerSpecific extends ParamEnum {
     public function __construct(?Deriveable $base, ?Collection $parent, string $key = "", string $label = "", string $description = "") {
         parent::__construct($base, $parent, $key, $label, $description, "", "");
 
-        new EnumItem($this, "SI1", _("SI-System [kg/W]"));
-        new EnumItem($this, "SI2", _("SI-System [W/kg]"));
+        new EnumItem($this, "SI1", _("SI-System [W/kg]"));
+        new EnumItem($this, "SI2", _("SI-System [kg/W]"));
         new EnumItem($this, "OLD1", _("Old-Stylish [kg/PS]"));
         new EnumItem($this, "OLD2", _("Old-Stylish [PS/t]"));
         $this->setValue("SI1");
